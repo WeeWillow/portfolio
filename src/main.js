d4629e8baf11c5354ba56cec670568a2bb21c53d
WeeWillow <weecalamy@gmail.com>
2024-10-21 20:12:04 +0200

    npm import material icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// material icons import
+import 'material-icons/iconfont/material-icons.css';
+
 const app = createApp(App)
 
 app.use(router)
