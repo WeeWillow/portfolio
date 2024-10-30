dcc637ca142f2d7a92bf15ded9a435120e642b59
Monica <weecalamy@gmail.com>
2024-10-30 13:40:44 +0100

    notes for an idea

---
+++ src/views/SmallWorkView.vue
@@ -0,0 +1,4 @@
+<!-- an overview of all frontend master challenges -->
+ <!-- shall have a grid/gallery with each challenge -->
+  <!-- maybe a component view where it changes the challenge -->
+  <!-- a window with the selected tab, or a "false" view, that dynamically changes when you click on a different challenge -->
