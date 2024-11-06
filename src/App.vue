7e410b0169a71fd92f2e617e24fb7643018359ec
WeeWillow <weecalamy@gmail.com>
2024-11-06 17:07:36 +0100

    updated to dynamically display header on home

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
 // import components
 import TheNav from '@/components/layout/TheNav.vue'
 import TheFooter from '@/components/layout/TheFooter.vue'
@@ -7,17 +8,17 @@ import ThemeButton from '@/components/ui/ThemeButton.vue'
 import TheHeader from './components/layout/TheHeader.vue';
 import ScrollToTop from './components/ui/ScrollToTop.vue';
 
-// const route = useRoute();
+const route = useRoute();
 
 // computed property check route matching path
-// const showTheHeader = computed(() => route.name === 'home')
+const showTheHeader = computed(() => route.name === 'home')
 </script>
 
 <template>
   <TheNav />
   <ThemeButton />
   <ScrollToTop />
-  <TheHeader />
+  <TheHeader v-if="showTheHeader" :routeName="route.name"/>
   <main>
     <router-view />
   </main>
