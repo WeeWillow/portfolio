764f593f63df168b1f57bfc63d052f2b32bd2bca
WeeWillow <weecalamy@gmail.com>
2024-11-13 23:49:06 +0100

    updated views

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import ContactView from '@/views/ContactView.vue'
-import ProjectGallery from '@/views/GalleryView.vue'
+import GalleryView from '@/views/GalleryView.vue'
 import ProjectDetail from '@/views/ProjectDetailView.vue'
 import ProjectOne from '@/views/ProjectOne.vue'
 
@@ -29,7 +29,7 @@ const router = createRouter({
     {
       path: '/projects',
       name: 'projects',
-      component: ProjectGallery,
+      component: GalleryView,
     },
     {
       path: '/project/:slug',
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import { cases } from '@/assets/data/case_data';
+import ProjectCard from '@/components/modules/ProjectCard.vue';
+
+
+const projects = ref(cases);
+
+</script>
+
+<template>
+<section class="projects-gallery">
+  <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+</section>
+</template>
+
+<style scoped lang="scss">
+.projects-gallery {
+  margin: var(--base-150) 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(var(--base-20x), 1fr));
+  gap: var(--base-200);
+}
+</style>
