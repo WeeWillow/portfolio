06ba5d43ca2d814e243bf75771b24de7f8b81135
WeeWillow <weecalamy@gmail.com>
2024-11-14 18:58:18 +0100

    added font awesome to main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,18 @@ import router from './router'
 
 // material icons import
 import 'material-icons/iconfont/material-icons.css';
+// font awesome library import - mainly for social icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+// font awesome icon component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import specific icons
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
+// add icons to library
+library.add(faGithub, faExternalLinkAlt)
 
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 app.mount('#app')
