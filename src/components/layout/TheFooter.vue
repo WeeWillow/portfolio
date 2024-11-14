d536555755097f105a4dd80a7d791a088242ad1f
WeeWillow <weecalamy@gmail.com>
2024-11-14 22:34:12 +0100

    styled footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,18 +1,37 @@
-<script setup>
-
-</script>
-
 <template>
 <footer>
-  <h2>Here's a footer</h2>
-  <p>&copy; 2024 - By Monica, all rights reserved.</p>
+  <img src="/assets/img/logo.png" alt="illustration of a dragon head">
+  <div class="content">
+    <h2>Monica Djurhuus</h2>
+
+    <p>&copy; 2024 - all rights reserved.</p>
+  </div>
 </footer>
 </template>
 
 <style scoped lang="scss">
 footer {
+  display: flex;
+  align-items: center;
+  gap: var(--base);
+  justify-content: center;
   background: var(--primary-200);
-  padding: var(--base-250);
-  text-align: center;
+  padding: var(--base-200);
+
+  img {
+    height: 100px;
+    width: auto;
+  }
+
+  h2 {
+    color: var(--primary-700);
+    font-weight: 900;
+  }
+  
+  p {
+    color: var(--primary-700);
+    font-size: var(--base);
+    font-weight: 300;
+  }
 }
 </style>
