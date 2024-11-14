440a8f7884639b8267e50370c5098fd24ed0f939
WeeWillow <weecalamy@gmail.com>
2024-11-14 22:33:00 +0100

    removed unused files

--- src/views/SmallWorkView.vue
+++
@@ -1,4 +0,0 @@
-<!-- an overview of all frontend master challenges -->
- <!-- shall have a grid/gallery with each challenge -->
-  <!-- maybe a component view where it changes the challenge -->
-  <!-- a window with the selected tab, or a "false" view, that dynamically changes when you click on a different challenge -->
