0bfda400dfafb59fd5b3dd6430b01c75cc171d2c
WeeWillow <weecalamy@gmail.com>
2024-11-14 22:35:25 +0100

    updated data

--- src/assets/data/case_data.js
+++ src/assets/data/case_data.js
@@ -7,7 +7,7 @@ export const cases = [
     live_demo: 'https://mmd.ucn.dk/class/MMD-CSD-S23/10489563/MMD2/denBlaaFestival_sem2_gruppe07/index.html',
     details: {
       title: 'Den Blå Festival',
-      desc: 'Den Blå Festival er en gratis Jazz Festival, der årligt tager sted i Aalborg.',
+      desc: 'Den Blå Festival er en gratis Jazz Festival, der årligt tager sted i Aalborg. De tilbyder et bredt udvalg af koncerter, og et trykt program, der også findes som PDF online.',
     },
     tags: {
       roles: ['UX/UI', 'Frontend'],
@@ -19,6 +19,45 @@ export const cases = [
       solution: 'For at imødekomme brugernes behov udviklede vi en interaktiv webapp, der supplerer det trykte program. Webappen giver festivalgæsterne et brugervenligt overblik over hele programmet. Ved hjælp af søgefunktioner kan brugerne nemt finde de koncerter, der interesserer dem, og sammensætte deres egen personlige festivalplan. Webappen er tilgængelig både før og under festivalen.',
       img: 'den-blaa-festival-img.png',
     },
+    articles: [
+      {
+        id: 1,
+        title: 'Project Overview',
+        sections: [
+          {
+            id: 1,
+            title: 'Introduction',
+            content: 'This is the introduction to the project.'
+          },
+          {
+            id: 2,
+            title: 'Details',
+            content: 'This section contains project details.'
+          },
+          {
+            id: 3,
+            title: 'Conclusion',
+            content: 'This is the conclusion of the project.'
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'User Research',
+        sections: [
+          {
+            id: 4,
+            title: 'Methodology',
+            content: 'This section explains the user research methodology used.'
+          },
+          {
+            id: 5,
+            title: 'Findings',
+            content: 'This section outlines the findings from user research.'
+          },
+        ],
+      },
+    ],
   },
   {
     id: 1,
@@ -28,7 +67,7 @@ export const cases = [
     live_demo: 'https://cine.weewillow.com/',
     details: {
       title: 'Cine Seekers',
-      desc: 'Cine Seekers er en fiktiv filmdatabase, udviklet som del af en praktikansøgningsprocess. Kunden har et ',
+      desc: 'Cine Seekers er en fiktiv filmdatabase, udviklet som del af en praktikansøgningsprocess. Klienten har et simpelt API, brugt til at hente data.',
     },
     tags: {
       roles: ['Frontend', 'UX/UI'],
@@ -36,10 +75,49 @@ export const cases = [
       approach: ['Mobile-first'],
     },
     intro: {
-      case: '',
-      solution: '',
+      case: 'Jeg har udviklet en hjemmeside for en klient, der ønsker at vise film online. Klienten har allerede et simpelt API, der fungerer som mellemled mellem hjemmesiden og dataleverandøren. Dataleverandøren ejer alle klientens data, herunder film, produkter og medieindhold.',
+      solution: 'Jeg har udviklet en responsiv hjemmeside, der præsenterer et overskueligt overblik over trending film. Hjemmesiden er bygget med Vue.js 3, og jeg har benyttet Vite som build-tool og Vue Router til navigation. Designet er skræddersyet med HTML og CSS. Ved at klikke på en film, ledes brugeren til en detaljeret side med relevant information hentet direkte fra API\'et.',
       img: 'cine-seekers-img.png',
     },
+    articles: [
+      {
+        id: 1,
+        title: 'Project Overview',
+        sections: [
+          {
+            id: 1,
+            title: 'Introduction',
+            content: 'This is the introduction to the project.'
+          },
+          {
+            id: 2,
+            title: 'Details',
+            content: 'This section contains project details.'
+          },
+          {
+            id: 3,
+            title: 'Conclusion',
+            content: 'This is the conclusion of the project.'
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'User Research',
+        sections: [
+          {
+            id: 4,
+            title: 'Methodology',
+            content: 'This section explains the user research methodology used.'
+          },
+          {
+            id: 5,
+            title: 'Findings',
+            content: 'This section outlines the findings from user research.'
+          },
+        ],
+      },
+    ],
   },
   {
     id: 2,
@@ -57,8 +135,8 @@ export const cases = [
       approach: ['Desktop-first'],
     },
     intro: {
-      case: 'noget om casen',
-      solution: 'hvordan løste vi det',
+      case: 'noget om casen. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum distinctio amet, debitis reprehenderit eum quasi soluta harum perferendis ea quisquam? Iure quo, quasi at molestiae quas qui? Recusandae, expedita soluta.',
+      solution: 'hvordan løste vi det. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum distinctio amet, debitis reprehenderit eum quasi soluta harum perferendis ea quisquam? Iure quo, quasi at molestiae quas qui? Recusandae, expedita soluta.',
       img: 'sejr-davidsen-img.png',
     },
     articles: [
